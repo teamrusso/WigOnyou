bcb53fab51d2cb7b2ae273240221e0aa2062df50
Matt Russo <Matt@macbookpro.mynetworksettings.com>
2025-03-30 20:55:35 -0400

    Fix: added port binding for Render deploy

--- app.py
+++ app.py
@@ -22,5 +22,8 @@ def upload():
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     return redirect('/')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
